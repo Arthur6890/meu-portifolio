1b70341f731e094cc275bc962030a78b165d745a
Arthur Amaral <arthur.ramos.amaral@hotmail.com>
2022-12-17 16:43:54 -0300

    adicionei 2 novos projetos, adicionei uma linha a mais no card de projetos que contem as linguagens usadas

--- components/myProjects/accordionData.ts
+++ components/myProjects/accordionData.ts
@@ -1,15 +1,34 @@
 import Flash from "../../public/images/flash.webp"
 import HandBala from "../../public/images/handbala.png"
+import Paredao from "../../public/images/paredaoLogo.png"
+import Filepao from "../../public/images/filepao8bit.png"
 
 export const AccordionData = [{
 	title: "flash site",
-	content: "This is a website the I developed at my current company, Flash Benefícios, located in São Paulo-Brasil. Unfortunately, I can't provide the code cuz it is professional, but you can see the site by the link available on the header",
+	content: "This is a website the I developed at my current company, Flash Benefícios, located in São Paulo-Brasil. Unfortunately, I can't provide the code cuz it is a professional job, restricted to Flash's employees, but you can see the site by the link available on the header",
 	link: "https://flashapp.com.br",
+	technologyUsed: "Next.js, Typescript, Sass, Vitest",
 	brand: Flash
 },
 {
 	title: "hand bet",
 	content: "This is a bet website, made using the UFMG's Exact Sciences Institute Handball's team as inspiration. In this website you can register your bet, see the line-ups, download your bet proof and more! All the website is responsible for the most used screens and smartphones!",
 	link: "https://hand-bet.vercel.app",
+	technologyUsed: "Next.js, Typescript, Sass",
 	brand: HandBala
-}]
+},
+{
+	title: "paredao gol-a-gol",
+	content: "This is a website created for Paredão Academia de Goleiros (Paredao Goalkeeper Academy) located in Belo Horizonte. Paredão is a great and consolidated goalkeeper academy with practices for goalies of all ages, helping them with physical e technical practices in order to help them to reach their dream to become a professional goalkeeper. All the website is responsible for the most used screens and smartphones!",
+	link: "https://paredao-gol-a-gol.vercel.app/",
+	technologyUsed: "Next.js, Typescript, Sass",
+	brand: Paredao,
+},
+{
+	title: "the uge app",
+	content: `This is a game developed with React Native. This game is an allusion to Flappy Bird, but using some content from my friends' group, named "UGE". That's why there are some contents and assets that may not make sense to some users. As I developed this game only for fun and academic purposes, I didn't upload it to PlayStore or Apple Store, so the redirect icon in the upper-right corner will redirect you directly to the GitHub repository of the game`,
+	link: "https://github.com/Arthur6890/the-uge-app",
+	technologyUsed: "React Native, Typescript",
+	brand: Filepao,
+},
+]
--- components/myProjects/index.tsx
+++ components/myProjects/index.tsx
@@ -10,11 +10,21 @@ import { AccordionData } from "./accordionData"
 
 export function MyProjects() {
 	return (
-		<div>
-
+		<>
 			<h2 className={styles.title}>
 				my main projects
 			</h2>
+			<h3 className={styles.subTitle}>
+				All the source codes are available on my
+				<Link href={"https://github.com/Arthur6890"}>
+					<a target="_blank">
+						Github Account
+					</a>
+				</Link>
+			</h3>
+			<span className={styles.exception}> (except for the Flash Website which why is explained on the respective card)</span>
+
+
 
 			{AccordionData.map((item, index) => (
 				<Accordion className={styles.accordion} key={index}>
@@ -39,11 +49,19 @@ export function MyProjects() {
 							<span className={styles.accordionContentText}>
 								{item.content}
 							</span>
+							<div className={styles.technologyUsed}>
+								<span className={styles.technologyUsedTitle}>
+									Languages/Frameworks:
+								</span>
+								<span className={styles.technologyName}>
+									{item.technologyUsed}
+								</span>
+							</div>
 						</div>
 
 					</AccordionDetails>
 				</Accordion>
 			))}
-		</div>
+		</>
 	)
 }
